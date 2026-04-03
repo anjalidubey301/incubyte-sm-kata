4c26d672ecb944887c46e0e49c6c2d1289b008c2
Anjali Dubey <anjalidubey76650@gmail.com>
2026-04-03 20:26:49 +0530

    Add test for create employee

--- spec/requests/employees_spec.rb
+++ spec/requests/employees_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe "Employees", type: :request do
-  describe "GET /employees" do
-    it "works! (now write some real specs)" do
-      get employees_index_path
-      expect(response).to have_http_status(200)
+
+  describe "POST /employees" do
+    let(:employee_params) do
+    {
+      employee: {
+        full_name: "Anjali",
+        job_title: "Developer",
+        country: "India",
+        salary: 50000
+      }
+    }
+  end
+    it "creates employee" do
+      post "/employees", params: employee_params
+      expect(response).to have_http_status(:created)
     end
   end
 end
