7cb1a1ab017705f19ad7e96712d95d9383f39586
Anjali Dubey <anjalidubey76650@gmail.com>
2026-04-03 21:46:14 +0530

    Add test for job title salary metrics

--- spec/requests/metrics_spec.rb
+++ spec/requests/metrics_spec.rb
@@ -16,4 +16,18 @@ RSpec.describe "Metrics", type: :request do
       expect(json["average"]).to eq(150.0)
     end
   end
+
+  describe "GET /metrics/job_title" do
+    it "returns average salary by job title" do
+      Employee.create!(full_name: "A", job_title: "Dev", country: "India", salary: 100)
+      Employee.create!(full_name: "B", job_title: "Dev", country: "US", salary: 300)
+
+      get "/metrics/job_title?job_title=Dev"
+
+      expect(response).to have_http_status(:ok)
+      json = JSON.parse(response.body)
+
+      expect(json["average"]).to eq(200.0)
+    end
+  end
 end
