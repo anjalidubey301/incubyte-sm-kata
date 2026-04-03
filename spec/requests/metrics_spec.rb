8e2dc0789f00852324d1999ee3975827a293c2e8
Anjali Dubey <anjalidubey76650@gmail.com>
2026-04-03 21:52:57 +0530

    Refactor Metrics controller and specs for DRY and readability

--- spec/requests/metrics_spec.rb
+++ spec/requests/metrics_spec.rb
@@ -1,33 +1,41 @@
 require 'rails_helper'
 
-RSpec.describe "Metrics", type: :request do
-   describe "GET /metrics/country" do
-    it "returns min, max, avg salary for country" do
-      Employee.create!(full_name: "A", job_title: "Dev", country: "India", salary: 100)
-      Employee.create!(full_name: "B", job_title: "Dev", country: "India", salary: 200)
+RSpec.describe "Metrics API", type: :request do
+  let(:base_attributes) do
+    {
+      full_name: "Employee",
+      job_title: "Dev",
+      salary: 100
+    }
+  end
 
+  def json_response
+    JSON.parse(response.body)
+  end
+
+  describe "GET /metrics/country" do
+    let!(:employee1) { Employee.create!(base_attributes.merge(country: "India", salary: 100)) }
+    let!(:employee2) { Employee.create!(base_attributes.merge(country: "India", salary: 200)) }
+
+    it "returns min, max, avg salary for country" do
       get "/metrics/country?country=India"
 
       expect(response).to have_http_status(:ok)
-      json = JSON.parse(response.body)
-
-      expect(json["min"]).to eq(100)
-      expect(json["max"]).to eq(200)
-      expect(json["average"]).to eq(150.0)
+      expect(json_response["min"]).to eq(100)
+      expect(json_response["max"]).to eq(200)
+      expect(json_response["average"]).to eq(150.0)
     end
   end
 
   describe "GET /metrics/job_title" do
-    it "returns average salary by job title" do
-      Employee.create!(full_name: "A", job_title: "Dev", country: "India", salary: 100)
-      Employee.create!(full_name: "B", job_title: "Dev", country: "US", salary: 300)
+    let!(:employee1) { Employee.create!(base_attributes.merge(country: "India", salary: 100)) }
+    let!(:employee2) { Employee.create!(base_attributes.merge(country: "US", salary: 300)) }
 
+    it "returns average salary by job title" do
       get "/metrics/job_title?job_title=Dev"
 
       expect(response).to have_http_status(:ok)
-      json = JSON.parse(response.body)
-
-      expect(json["average"]).to eq(200.0)
+      expect(json_response["average"]).to eq(200.0)
     end
   end
 end
