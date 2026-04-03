f9f096e8e163c62f50cb7c9077dcd527ab5b2e77
Anjali Dubey <anjalidubey76650@gmail.com>
2026-04-03 19:43:45 +0530

    Fixed uninitialized class variable @@accept_charset

--- config/boot.rb
+++ config/boot.rb
@@ -1,3 +1,16 @@
+# config/boot.rb
+
+# ===============================
+# Patch for Ruby 3.4 CGI issue
+# Fixes "uninitialized class variable @@accept_charset"
+require 'cgi'
+class CGI
+  unless defined?(@@accept_charset)
+    @@accept_charset = nil
+  end
+end
+# ===============================
+
 ENV["BUNDLE_GEMFILE"] ||= File.expand_path("../Gemfile", __dir__)
 
 require "bundler/setup" # Set up gems listed in the Gemfile.
