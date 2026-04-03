e403c32d62bac88c0b1af15c5a805d0d7f60d311
Anjali Dubey <anjalidubey76650@gmail.com>
2026-04-03 21:21:54 +0530

    Add test for salary calculation for India

---
+++ spec/requests/salaries_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+RSpec.describe "Salaries", type: :request do
+  describe "GET /employees/:id/salary" do
+    it "calculates salary for India" do
+      emp = Employee.create!(
+        full_name: "A",
+        job_title: "Dev",
+        country: "India",
+        salary: 1000
+      )
+
+      get "/employees/#{emp.id}/salary"
+
+      expect(response).to have_http_status(:ok)
+      json = JSON.parse(response.body)
+
+      expect(json["gross"]).to eq(1000)
+      expect(json["deduction"]).to eq(100)
+      expect(json["net"]).to eq(900)
+    end
+  end
+end
